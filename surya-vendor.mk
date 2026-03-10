3ceb7508ac9e9d5521a4fd2d460fdd21966a06cf
Raya Mahardika <rayapkun@hlcyn.co>
2026-03-10 04:10:00 +0000

    surya: Update Display Stack from AOSPA
    Change-Id: I350b488ae25309d1a92f4d22bdb6cd1063b70508

--- surya-vendor.mk
+++ surya-vendor.mk
@@ -518,11 +518,9 @@ PRODUCT_PACKAGES += \
     libconfigdb \
     libcpion \
     libdataitems \
-    libdisp-aba \
     libdisplayqos \
     libdng_sdk2vendor \
     libdpmqmihal \
-    libdpps \
     libdrmfs \
     libdrmtime \
     libdsi_netctrl \
